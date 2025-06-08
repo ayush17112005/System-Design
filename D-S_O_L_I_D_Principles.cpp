c4c18722ffac7fbee8e1af2dbf93291b195c7ed5
ayush17112005 <saxenaayushman99@gmail.com>
2025-06-08 12:59:52 +0530

    Factory Pattern

---
+++ D-S_O_L_I_D_Principles.cpp
@@ -0,0 +1,8 @@
+// ----------------- S.O.L.I.D Principles -------------------
+// D - Dependency Inversion Principle (DIP)
+// High-level modules should not depend on low-level modules. Both should depend on abstractions.
+// Abstractions should not depend on details. Details should depend on abstractions.
+
+#include <iostream>
+#include <memory>
+using namespace std;
---
+++ Factory_Pattern.cpp
@@ -0,0 +1,57 @@
+// ---------------------------- S.O.L.I.D Principles ----------------------------
+// Factory Pattern Example
+#include <bits/stdc++.h>
+using namespace std;
+
+class ShapeInterface
+{
+public:
+    virtual void Draw() = 0; // Pure virtual function for drawing the shape
+};
+
+class Circle : public ShapeInterface
+{
+public:
+    void Draw() override
+    {
+        cout << "Drawing Circle" << endl;
+    }
+};
+
+class Square : public ShapeInterface
+{
+public:
+    void Draw() override
+    {
+        cout << "Drawing Square" << endl;
+    }
+};
+class ShapeFactory
+{
+public:
+    // Factory method to create shapes based on string input
+    ShapeInterface *GetShape(const string &str)
+    {
+        if (str == "Circle")
+            return new Circle();
+        if (str == "Square")
+            return new Square();
+    }
+};
+
+int main()
+{
+    ShapeFactory factory;
+    string shape_;
+    cin >> shape_;
+    ShapeInterface *shape1 = factory.GetShape(shape_);
+    shape1->Draw();
+    // here user is prompted to enter the shape type (Circle or Square)
+    // we dont know at compile time which shape will be created
+    // so we use a factory method to create the shape dynamically based on user input.
+
+    // Clean up
+    delete shape1;
+
+    return 0;
+}
